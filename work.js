9ef7ddc3a020c5a27f82a87610cbc430b72ee0db
Tim Henrich <tim.henrich@keytech.de>
2020-10-30 15:01:47 +0100

    Initial

---
+++ work.js
@@ -0,0 +1,4 @@
+document.getElementById("zwei").style.paddingLeft = 0;
+document.getElementById("zwei").style.paddingRight = '10%';
+document.getElementById("zwei").style.textAlign = 'right';
+document.getElementById("zwei").style.float = 'right';
