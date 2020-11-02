232304f7e9993e4a115efd033ab70591e50081f2
Timhnr <72263605+Timhnr@users.noreply.github.com>
2020-11-02 09:34:06 +0100

    Update work.js

--- work.js
+++ work.js
@@ -2,3 +2,30 @@ document.getElementById("zwei").style.paddingLeft = 0;
 document.getElementById("zwei").style.paddingRight = '10%';
 document.getElementById("zwei").style.textAlign = 'right';
 document.getElementById("zwei").style.float = 'right';
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
